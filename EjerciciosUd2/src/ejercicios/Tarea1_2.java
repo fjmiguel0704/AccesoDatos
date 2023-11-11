4ab2ad24bbeb856f1933e1d6bbda657210b4db83
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-11-11 23:38:19 +0100

    terminado

--- EjerciciosUd2/src/ejercicios/Tarea1_2.java
+++ EjerciciosUd2/src/ejercicios/Tarea1_2.java
@@ -1,25 +1,31 @@
 package ejercicios;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
 
 public class Tarea1_2 {
 	public static void main(String[] args) {
 		String lineaFichero = "";
 		String palabrasFichero = "";
-		int posInicioPalabra = 0;
-		int posFinalPalabra = 5;
 		
 		try {
 			BufferedReader leerFichero = new BufferedReader(new FileReader("src\\ejercicios\\ahorcado.txt"));
 
 			while ((lineaFichero = leerFichero.readLine()) != null) {
 				for (int i = 0; i<=lineaFichero.length(); i++) {
-					
+					palabrasFichero = lineaFichero.substring(i, i+5);
+					i+=4;
+					File newFile = new File("ficheros\\ahorcadoOrdenado.txt");
+					BufferedWriter bw = new BufferedWriter(new FileWriter(newFile, true));
+					bw.write(palabrasFichero + "\n");
+					bw.flush();
 				}
 			}
-			
-			System.out.println(palabrasFichero);
+
 				
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
