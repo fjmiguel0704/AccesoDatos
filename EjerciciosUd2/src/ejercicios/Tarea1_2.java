6da30ccc36e2c1932fcf475714b1a42d44d33949
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-13 09:27:28 +0100

    terminado

--- EjerciciosUd2/src/ejercicios/Tarea1_2.java
+++ EjerciciosUd2/src/ejercicios/Tarea1_2.java
@@ -9,24 +9,22 @@ import java.io.Writer;
 
 public class Tarea1_2 {
 	public static void main(String[] args) {
-		String lineaFichero = "";
-		String palabrasFichero = "";
+		String lineaFicheroAhorcadoOrdenado = "";
+		String palabrasFicheroAhorcadoOrdenado = "";
 		
 		try {
-			BufferedReader leerFichero = new BufferedReader(new FileReader("src\\ejercicios\\ahorcado.txt"));
-
-			while ((lineaFichero = leerFichero.readLine()) != null) {
-				for (int i = 0; i<=lineaFichero.length(); i++) {
-					palabrasFichero = lineaFichero.substring(i, i+5);
-					i+=4;
-					File newFile = new File("ficheros\\ahorcadoOrdenado.txt");
-					BufferedWriter bw = new BufferedWriter(new FileWriter(newFile, true));
-					bw.write(palabrasFichero + "\n");
+			BufferedReader leerFicheroAhorcado = new BufferedReader(new FileReader("src\\ejercicios\\ahorcado.txt"));
+			lineaFicheroAhorcadoOrdenado = leerFicheroAhorcado.readLine();
+			
+			File newFileAhorcadoOrdenado = new File("ficheros\\ahorcadoOrdenado.txt");
+			
+				for (int i = 0; i<=lineaFicheroAhorcadoOrdenado.length(); i+=5) {
+					palabrasFicheroAhorcadoOrdenado = lineaFicheroAhorcadoOrdenado.substring(i, i+5);				
+					BufferedWriter bw = new BufferedWriter(new FileWriter(newFileAhorcadoOrdenado, true));
+					bw.write(palabrasFicheroAhorcadoOrdenado + "\n");
 					bw.flush();
 				}
-			}
-
-				
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
