40b1c47f091d968d60cef6a8b41f35b8faa45ada
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-17 11:55:01 +0100

    terminado

--- EjerciciosUd2/src/ejercicios/Tarea1_2b.java
+++ EjerciciosUd2/src/ejercicios/Tarea1_2b.java
@@ -5,23 +5,35 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class Tarea1_2b {
 
 	public static void main(String[] args) {
-		String lineaFicheroAhorcadoOrdenadoAscendente = "";
-		String palabrasFicheroAhorcadoOrdenadoAscendente = "";
+		String linea = "";
+		ArrayList<String> ahorcadoOrdenadoAlfabeticamente = new ArrayList<String>();
 		
 		
 		try {
-			BufferedReader leerFicheroAhorcadoOrdenado = new BufferedReader(new FileReader("ficheros\\ahorcadoOrdenado.txt"));
-			lineaFicheroAhorcadoOrdenadoAscendente = leerFicheroAhorcadoOrdenado.readLine();
+			BufferedReader br = new BufferedReader(new FileReader("ficheros\\ahorcadoOrdenado.txt"));
 			
-			File newFileAhorcadoOrdenadoAscendente = new File("ficheros\\ahorcadoOrdenadoAscendente.txt");
+			File newFileAhorcadoOrdenadoAlfabeticamente = new File("ficheros\\ahorcadoOrdenadoAscendente.txt");
 			
-			while ((lineaFicheroAhorcadoOrdenadoAscendente = leerFicheroAhorcadoOrdenado.readLine())!=null) {
-				
+			BufferedWriter bw = new BufferedWriter(new FileWriter(newFileAhorcadoOrdenadoAlfabeticamente, false));
+			
+			while ((linea = br.readLine())!=null) {
+				ahorcadoOrdenadoAlfabeticamente.add(linea);
 			}
+			
+			ahorcadoOrdenadoAlfabeticamente.sort(null);
+			
+			for (String palabras : ahorcadoOrdenadoAlfabeticamente) {
+				bw.write(palabras + "\n");
+				bw.flush();
+			}
+			
+			br.close();
+			bw.close();
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
