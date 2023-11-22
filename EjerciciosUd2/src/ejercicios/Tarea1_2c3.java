a70d2116f87d64b430b34567c3b8c3bb50b194ed
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-11-22 09:31:16 +0100

    terminado

--- EjerciciosUd2/src/ejercicios/Tarea1_2c3.java
+++ EjerciciosUd2/src/ejercicios/Tarea1_2c3.java
@@ -2,31 +2,32 @@ package ejercicios;
 
 import java.io.RandomAccessFile;
 
-public class Tarea1_2c2 {
+public class Tarea1_2c3 {
 
 	public static void main(String[] args) {
 		String ficheroA = "..\\EjerciciosUd2\\ficheros\\ficheroA.txt";
-		String ficheroEA = "..\\EjerciciosUd2\\ficheros\\ficheroEA.txt";
+		String ficheroEANumeros = "..\\EjerciciosUd2\\ficheros\\ficheroEANumeros.txt";
 		String caracter;
 		
 		try {
 			RandomAccessFile fileA = new RandomAccessFile(ficheroA, "r");
-			RandomAccessFile fileEA = new RandomAccessFile(ficheroEA, "rw");
+			RandomAccessFile fileEANumber = new RandomAccessFile(ficheroEANumeros, "rw");
 			
 			
-			for (int i = (int) fileA.length()-3; i>=0; i-=3) {
+			for (int i = (int) fileA.length()-4; i>=0; i-=4) {
 				fileA.seek(i);
 				caracter = fileA.readLine();
-				fileEA.writeBytes(caracter + "\n");
+				fileEANumber.writeBytes(caracter + "\n");
 			}
 			
 			fileA.close();
-			fileEA.close();
+			fileEANumber.close();
 			
 			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+
 	}
 
 }
